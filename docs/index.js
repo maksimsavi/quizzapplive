c5aca69f24674118b2e7bbb8db0998168c113da6
maksimsavi <savianokm@gmail.com>
2020-05-15 00:00:44 -0500

    final with animations

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,4 @@
+//question data
 const dataArray = [
 {country: 'Italy',
  choices: ['Rome', 'Budapest', 'Florence'],
@@ -15,67 +16,51 @@ const dataArray = [
  choices: ['Budapest', 'Gyor', 'Pecs'],
  correct: 'Budapest'},
 ];
+// globally mutated array which keeps track of variables
 const counterObject = {
     globalScoreCount: 1,
     arrayIndex: 0,
     correctCount: 0,
-    incorrectCount: 0
+    incorrectCount: 0,
+    userInput: null
 }
-/*
-let globalScoreCount = 1;
-let arrayIndex =0;
-let correctCount = 0;
-let incorrectCount = 0;
-*/
-
-// two lines below are from renderQuestion function, check if they will mutate properly
-
+//resets variables, renders first question
 function startQuizz() {
-    //render the quiz section, which will start with init question
-    //click button, run renderQuestion()
-    //sets global counter to 0, check if these work properly
-    //let globalScoreCount = 1;
     counterObject.globalScoreCount = 1;
-    //let correctCount = 0;
     counterObject.correctCount = 0;
-    // let incorrectCount = 0;
     counterObject.incorrectCount = 0;
     counterObject.arrayIndex = 0;
     console.log('startQuizz ran');
     $("#startButton").click(function() {
-        renderQuestion();
+        checkWhatToLoad();
       });
 }
+//check if answer is correct and reacts accordingly
 function evaluateAnswer() {
-    $( "#quizzForm" ).submit(function( event ) {
-        event.preventDefault();
-        userInput = $("input[name='city']:checked").val();
-            if (userInput === dataArray[counterObject.arrayIndex].correct) {
-            console.log('correct');
-            counterObject.correctCount++;
-            $(showCorrect());
-        } else {
-            console.log('nah b');
-            counterObject.incorrectCount++;
-            $(showIncorrect());
-           }
-      //  };
-       // ifElseStatement();
-        
-         
-      });
-    //increment counter;
-    
-    //if right show the right screen()
-    //if wrong show the wrong screen()
-    //in both scenarios, next button will do the same thing - render next question.
-    console.log('evaluateAnswer ran')
+    if (counterObject.userInput === dataArray[counterObject.arrayIndex].correct) {
+        console.log('correct');
+        counterObject.correctCount++;
+        $(showCorrect());
+    } else {
+        console.log('nah b');
+        counterObject.incorrectCount++;
+        $(showIncorrect());
+       }
     
 }
+//handles question answer (collects it, and runs evaluation function)
+function collectAnswer() {
+    $('main').on( "submit","#quizzForm",(function() {
+        event.preventDefault();
+        counterObject.userInput = $("input[name='city']:checked").val();
+        evaluateAnswer();
+      }));
+}
+//renders incorrect screen, updates score
 function showIncorrect(){
-    //shows incorrect screens, on 'next' click runs renderQuestion
+    
     $('main').html(`
-    <section class="quizzIncorrect">
+    <section class="quizzIncorrect fadeInAnimation">
     <h2>Oops! Capital of ${dataArray[counterObject.arrayIndex].country} is ${dataArray[counterObject.arrayIndex].correct}.</h2>
     <button type="button" id="nextButton">Next</button>
     `);
@@ -83,38 +68,38 @@ function showIncorrect(){
     counterObject.arrayIndex++;
     
 }
+//renders correct screen, updates score
 function showCorrect(){
     //shows correct screens, on 'next' click runs renderQuestion
     $('main').html(`
-    <section class="quizzIncorrect">
+    <section class="quizzIncorrect fadeInAnimation">
     <h2>Yes! Capital of ${dataArray[counterObject.arrayIndex].country} is indeed 
     ${dataArray[counterObject.arrayIndex].correct}.</h2>
     <button type="button" id="nextButton">Next</button>
     `);
     counterObject.globalScoreCount++;
     counterObject.arrayIndex++;
- }
-function reactToNextButtonClick() {
-    $('main').on( "click","#nextButton",(function() {
-        
-        renderQuestion();
-      }));
-
-      console.log('react to next ran');
 }
-function renderQuestion() {
-    //if dataArray.length === dataArray.length+1, then load final page??
-    //
-    //looks at global counter, renders the question page substituting proper values.
-    //global counter shows question count, and 
-    
+//tells browser to render next question
+function reactToNextButtonClick() {
+    $('main').on( "click","#nextButton",(function() { 
+        checkWhatToLoad();
+        }));
+}
+//check what browser should load
+function checkWhatToLoad() {
     if (counterObject.globalScoreCount === dataArray.length+1) {
         $(renderEnd());
     }
     else {
-        
+        renderQuestion();
+}
+}
+//renders a question
+
+function renderQuestion() {
     $('main').html(`
-    <section class="quizz">
+    <section class="quizz fadeInAnimation">
     <form id="quizzForm">
         <fieldset>
             <legend><h2>${counterObject.globalScoreCount}/${dataArray.length}: What is the capital of 
@@ -130,38 +115,38 @@ function renderQuestion() {
 
             
             <label for="${dataArray[counterObject.arrayIndex].choices[2]}"><div class="answerOption"><input class="design" type="radio" id="${dataArray[counterObject.arrayIndex].choices[2]}" name="city" value="${dataArray[counterObject.arrayIndex].choices[2]}">${dataArray[counterObject.arrayIndex].choices[2]}</div></label>
-            <div class="inputholder"><input type="submit" value="check"></div>
+            <div class="inputholder"><input id="checkButton" type="submit" value="check"></div>
             </fieldset>    
         <p class="scorePar">Correct: ${counterObject.correctCount}, Incorrect: ${counterObject.incorrectCount}</p>
         
     </form>
 </section>
     `);
-    evaluateAnswer();}
+    
 }
+//renders final page
 function renderEnd() {
-    //this will render the final page, and listen for the 'try again button' which 
-    //will just run -startQuizz()-
     $('main').html(`
-    <section class="quizzEnd">
+    <section class="quizzEnd fadeInAnimation">
     <h2 id="endTitle">The End!</h2>
     <button type="button" id="tryAgainButton">try again?</button>
     <p class="scorePar">Correct: ${counterObject.correctCount}, Incorrect: ${counterObject.incorrectCount}</p>
     </section>
     `);
 }
+// restarts quizz
 function reactToTryAgainButtonClick() {
     $('main').on( "click","#tryAgainButton",(function() {
         $(startQuizz());
-        $(renderQuestion);
+        $(checkWhatToLoad());
       }));
-
-      console.log('react to try again ran');
 }
+//all the listeners
 function runQuizApp(){
     startQuizz(); 
     reactToNextButtonClick();  
     reactToTryAgainButtonClick();
-    
+    collectAnswer();
 }
+//initiates system launch lol am I cool now
 $(runQuizApp);
